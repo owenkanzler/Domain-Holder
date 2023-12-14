6be3dba14351f50794661fc78de5ce49f81a61f0
Owen Kanzler <oj.kanz1@gmail.com>
2023-12-13 21:09:48 -0600

    another test

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: "/",
+//   plugins: [react()],
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/Domain-Holder/",
-  plugins: [react()],
+// https://vitejs.dev/config
+export default defineConfig(() => {
+  const config = {
+    base: `/`,
+    plugins: [react()],
+    esbuild: {
+      jsxFactory: "React.createElement",
+      jsxFragment: "React.Fragment",
+    },
+  };
+
+  return config;
 });
